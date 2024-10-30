f34a80cef44445e6bf65e43e55edbd0a355676d0
amirmasoud asaditalab <mramirmasoudprogrammer.com>
2024-10-30 20:00:55 +0330

    edit img

--- src/components/module/CardProducts.jsx
+++ src/components/module/CardProducts.jsx
@@ -4,7 +4,7 @@ import svg1 from "@/public/Frame 40.png"
 import Image from "next/image";
 import styles from "@/src/styles/StylesCardProducts.module.css"
 import {Text} from "@/src/helper/Help";
-import imgdata2 from "@/public/UsFIvYs.jpeg"
+
 
 function CardProducts({data}) {
     const {title, price, images } = data
@@ -13,7 +13,7 @@ function CardProducts({data}) {
 
     return (
         <Link className={styles.Card} href={`/Categories/${data.id}`}>
-            <Image src={images[0] ? images[0] : imgdata2} width={280} height={280} alt="image" priority={true}/>
+            <Image src={images[0]} width={280} height={280} alt="image" priority={true}/>
             <span>{Text(title)}</span>
             <div className={styles.icons}>
                 <Image src={svg1} alt="icon" width={113} height={19} priority={true}/>
